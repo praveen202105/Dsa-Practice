b8cb4410ef32919ed15a7e7f354f89b2df75052e
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2025-06-01 16:15:02 +0530

    [930] [Time Beats: 22.28%] [Memory Beats: 10.81%] - LeetPush

--- Binary-Subarrays-With-Sum.cpp
+++ Binary-Subarrays-With-Sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int totalsubarray(vector<int>&nums,int goal){
-        int i=0,j=0,ans=0,sum=0;
+    int numSubarraysWithSum(vector<int>& arr, int target) {
+       unordered_map<int,int>mp;
+        int s=0,ans=0;
         
-        while(j<nums.size()){
-            sum+=nums[j];
-    
-            while(sum>goal && i<=j){
-                sum-=nums[i];
-                i++;
-            }
-
-            ans+=((j-i)+1);
-            j++;
+        for(auto i:arr){
+            s+=i; 
+            if(s==target)ans++; 
+            ans+=mp[s-target];
+            mp[s]++;
+            
         }
-
+        
         return ans;
-    }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return totalsubarray(nums,goal)-totalsubarray(nums,goal-1);
-        //total subarray upto sum goal -total subaarray upto sum goal-1
     }
 };
