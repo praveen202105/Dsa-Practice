8d58dccd9145a31461a24c3c6d5688b5e184085d
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2025-06-23 09:08:14 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 90.67%] - LeetPush

--- Unique-Paths-II.cpp
+++ Unique-Paths-II.cpp
@@ -13,34 +13,38 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size(),n=obstacleGrid[0].size();
         if(obstacleGrid[0][0]) return 0;
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         // for(int i=0;i<n;i++){
         //     // if(obstacleGrid[0][i]==0)
-            dp[0][0]=1;
+            // dp[0][0]=1;
         // }
-     
+      vector<int>prev(n+1,0);
+    //   prev[0]=1;
 //  dp[0][0]=1;
         for(int i=0;i<m;i++){
+            vector<int>curr(n+1,0);
+        //    curr[0]=1;
             for(int j=0;j<n;j++){    
-                if(i==0 & j==0) continue;
+                if(i==0 & j==0){curr[0]=1;
+                 continue;
+            }
                 if(obstacleGrid[i][j]==0){
                         int up=0;
-                        if(i)up=dp[i-1][j];
+                        if(i)up=prev[j];
                         int left=0;
-                        if(j)left=dp[i][j-1];
-                        cout<<i<<" "<<j<<" "<<up<<"+"<<left<<endl;
-                       dp[i][j]=up+left;
+                        if(j)left=curr[j-1];
+                        // cout<<i<<" "<<j<<" "<<up<<"+"<<left<<endl;
+                       curr[j]=up+left;
                 }
-
-
             }
+            prev=curr;
         }
         
         // for(auto i:dp){
         //     for(auto j:i) cout<<j<<" ";
         //     cout<<endl;
         // }
-        return dp[m-1][n-1];
+        return prev[n-1];
     
     }
 };
