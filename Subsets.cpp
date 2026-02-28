6b1e82a7ccdf92c892aab2116c53ff17eed4f705
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2026-02-28 14:56:05 +0530

    [78] [Time Beats: 30.44%] [Memory Beats: 27.33%] - LeetPush

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,22 +1,20 @@
-class Solution {
-public:
-    void findans(vector<int>&nums,int i,vector<int>&subans,vector<vector<int>>&ans){
-    if(i==nums.size()){
-        ans.push_back(subans);
-        return;
-    }
-
-    subans.push_back(nums[i]);
-    findans(nums,i+1,subans,ans);
-    subans.pop_back();
-        findans(nums,i+1,subans,ans);
-    
-
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-     vector<vector<int>>ans;
-     vector<int>subans;
-     findans(nums,0,subans,ans);  
-     return ans; 
-    }
-};
+1class Solution {
+2public:
+3void findans(int i,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+4    if(i==nums.size()){
+5        ans.push_back(temp);
+6        return;
+7    }
+8
+9    findans(i+1,nums,temp,ans);
+10    temp.push_back(nums[i]);
+11    findans(i+1,nums,temp,ans);
+12    temp.pop_back();
+13}
+14    vector<vector<int>> subsets(vector<int>& nums) {
+15            vector<vector<int>>ans;
+16            vector<int>temp;
+17            findans(0,nums,temp,ans);
+18            return ans;
+19    }
+20};
