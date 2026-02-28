55e2ee1caa6110e19e90287c36293113daa6b284
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2026-02-28 15:18:53 +0530

    [90] [Time Beats: 100.00%] [Memory Beats: 84.47%] - LeetPush

--- Subsets-II.cpp
+++ Subsets-II.cpp
@@ -1,25 +1,27 @@
-class Solution {
-public:
-    void findans(vector<int>&nums,int i,vector<int>&subans,vector<vector<int>>&ans){
-    if(i>nums.size()) return;
-    if(i==nums.size()){
-        ans.push_back(subans);
-        return;
-    }
-
-    subans.push_back(nums[i]);
-    findans(nums,i+1,subans,ans);
-    subans.pop_back();
-    while(i+1<nums.size() && nums[i]==nums[i+1])i++;
-        findans(nums,i+1,subans,ans);
-    
-
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-          vector<vector<int>>ans;
-     vector<int>subans;
-     findans(nums,0,subans,ans);  
-     return ans; 
-    }
-};
+1class Solution {
+2public:
+3void findans(int i,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+4    if(i==nums.size()){
+5        ans.push_back(temp);
+6        return;
+7    }
+8
+9    temp.push_back(nums[i]);
+10    findans(i+1,nums,temp,ans);
+11    temp.pop_back();
+12
+13    int j=i;
+14    while (j + 1 < nums.size() && nums[j] == nums[j + 1]) {
+15        j++;
+16    }
+17    findans(j+1,nums,temp,ans);
+18}
+19
+20    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+21        sort(nums.begin(),nums.end());
+22         vector<vector<int>>ans;
+23            vector<int>temp;
+24            findans(0,nums,temp,ans);
+25            return ans;
+26    }
+27};
