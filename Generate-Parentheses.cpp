0b1d6919651cec3ce6e7f791c1f73d3d9be2ffeb
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2026-03-01 00:21:30 +0530

    [22] [Time Beats: 70.42%] [Memory Beats: 51.82%] - LeetPush

--- Generate-Parentheses.cpp
+++ Generate-Parentheses.cpp
@@ -1,31 +1,19 @@
-class Solution {
-public:
-    void findans(vector<string>&ans,string s,int l,int r){
-        if(l==0 && r==0){
-            ans.push_back(s);
-             return;
-             }
-
-   if(l) {
-    s.push_back('(');
-   
-    findans(ans,s,l-1,r);
-    s.pop_back();
-    }
-
-       if(r>l) {
-           s.push_back(')');
-        findans(ans,s,l,r-1);
-        }
-        // s.pop_back();
- 
-       return ;
-
-    }
-    vector<string> generateParenthesis(int n) {
-     vector<string>ans;
-     string s;
-     findans(ans,s,n,n);
-     return ans;   
-    }
-};
+1class Solution {
+2public:
+3    void findans(string s,vector<string>&ans,int l,int r){
+4        if(l==0 && r==0){
+5            ans.push_back(s);
+6            return;
+7        }
+8
+9        if(l)findans(s+'(',ans,l-1,r);
+10        if(r>l)   findans(s+')',ans,l,r-1);
+11        return;
+12    }
+13    vector<string> generateParenthesis(int n) {
+14        vector<string>ans;
+15        string s;
+16        findans(s,ans,n,n);
+17        return ans;
+18    }
+19};
