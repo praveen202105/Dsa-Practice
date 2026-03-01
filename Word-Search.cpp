cd2b5074bb4950305bcd6a6a5f5940da6582f929
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2026-03-01 12:12:59 +0530

    [79] [Time Beats: 21.22%] [Memory Beats: 47.19%] - LeetPush

--- Word-Search.cpp
+++ Word-Search.cpp
@@ -1,42 +1,35 @@
 1class Solution {
 2public:
-3   bool isvalid(int itr,int r,int c,string word,vector<vector<int>>&visited,vector<vector<char>>& board){
-4      return r>=0 && c>=0 && r<visited.size() && c<visited[0].size() && visited[r][c]==0 && board[r][c]==word[itr]; 
-5  }
-6   bool findans(int itr,int r,int c,string word,vector<vector<int>>&visited,vector<vector<char>>& board){
-7       if(itr==word.size()){
-8           return true;
-9       }
-10         if (!isvalid(itr, r, c, word, visited, board))
-11        return false;
-12
-13      vector<int> rdir = {1, 0, 0, -1};
-14        vector<int> cdir = {0, -1, 1, 0};
+3  
+4   bool findans(int i,int j,int c,string word,vector<vector<char>>& board){
+5    if (c == word.size())
+6            {
+7                return true;
+8            }
+9    if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[c]) return false;
+10
+11  char org=board[i][j];
+12    board[i][j] = '#';
+13       
+14       bool found = (findans(i + 1, j, c + 1, word, board) || findans(i-1, j, c + 1, word,board) || findans(i, j + 1, c + 1, word,board) || findans(i , j-1, c + 1, word,board));
 15
-16    visited[r][c] = 1;
-17       
-18       for(int i=0;i<4;i++){
-19           int nr=r+rdir[i],nc=c+cdir[i];
-20            if (findans(itr + 1, nr, nc, word, visited, board))
-21            return true;
-22       }      
-23       
-24    visited[r][c] = 0;
-25       return false;
-26       
-27       
-28   }
-29   
-30    bool exist(vector<vector<char>>& board, string word) {
-31          int r=board.size()-1;
-32        int c=board[0].size()-1;
-33        vector<vector<int>>visited(r+1,vector<int>(c+1,0));
-34        for (int i = 0; i <=r; i++) {
-35            for (int j = 0; j <= c; j++) {
-36                if (findans(0,i, j, word, visited,board))
-37                    return true;
-38            }
-39        }
-40        return false;
-41    }
-42};
+16       
+17    board[i][j] = org;
+18       return found;
+19       
+20       
+21   }
+22   
+23    bool exist(vector<vector<char>>& board, string word) {
+24          int r=board.size();
+25        int c=board[0].size();
+26        // vector<vector<int>>visited(r+1,vector<int>(c+1,0));
+27        for (int i = 0; i <r; i++) {
+28            for (int j = 0; j < c; j++) {
+29                if (board[i][j] == word[0] && findans(i, j,0, word,board))
+30                    return true;
+31            }
+32        }
+33        return false;
+34    }
+35};
