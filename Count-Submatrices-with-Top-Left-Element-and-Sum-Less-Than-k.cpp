bcb161d6aa92db76df95327a3cf38e27a9eae9a5
praveen202105 <55058973+praveen202105@users.noreply.github.com>
2026-03-18 09:06:42 +0530

    [3070] [Time Beats: 43.26%] [Memory Beats: 82.02%] - LeetPush

--- Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-k.cpp
+++ Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-k.cpp
@@ -5,20 +5,14 @@
 5        int ans=0;
 6        for(int i=0;i<n;i++){
 7            for(int j=0;j<m;j++){
-8                if(i==0 && j){
-9                    grid[i][j]+=grid[i][j-1];
-10                }else if(j==0 && i){
-11                    grid[i][j]+=grid[i-1][j];
-12                }else if(i && j){
-13                    grid[i][j]+=grid[i-1][j];
-14                    grid[i][j]+=grid[i][j-1];
-15                    grid[i][j]-=grid[i-1][j-1];
-16                }
-17                if(grid[i][j]<=k) ans+=1;
-18            }
-19        }
-20       
-21       return ans;
-22
-23    }
-24};
+8                if(j) grid[i][j]+=grid[i][j-1];
+9                if(i)grid[i][j]+=grid[i-1][j];
+10                if(i && j)grid[i][j]-=grid[i-1][j-1];
+11                if(grid[i][j]<=k) ans+=1;
+12            }
+13        }
+14       
+15       return ans;
+16
+17    }
+18};
